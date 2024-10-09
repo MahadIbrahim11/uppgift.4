8fa35ad0520dc6f78385fdc13eaf475c223edba1
maib07001 <maib07001@E-PW05C37V>
2024-10-09 06:13:39 +0200

    Add project files.

---
+++ uppgift.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uppgift_4._1
+{
+    class Program
+    {
+      static void Main(string[] args)
+        {
+            Console.WriteLine("vad har de fösta anställda för lön");
+            int lön = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("vad har de andra anställda för lön");
+            int lön2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("vad har den tredje anställda för lön");
+            int lön3 = int.Parse(Console.ReadLine());
+
+            int medellönen = (lön * 600) - 600;
+            int lön4 = 200 + medellönen;
+
+            Console.WriteLine("medellönen är" + lön + "kronor");
+
+
+        }
+
+
+    }
+
+
+}
